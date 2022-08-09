275f46f1b2321d41ef786b9a7f0f7e976cbe3de6
RishiKiran-26 <rishikiran07@gmail.com>
2022-08-09 14:55:28 +0530

    Changes have been committed

---
+++ Phase4Section1.4/Phase4Section1.4/GoogleSearch.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+
+namespace Phase4Section1._4
+{
+    [TestClass]
+    public class GoogleSearch
+    {
+        [TestMethod]
+        public void Test()
+        {
+            using (var driver = new ChromeDriver("C:\\Users\\DELL\\OneDrive\\Desktop"))
+            {
+                driver.Manage().Window.Maximize();
+                driver.Navigate().GoToUrl("https://www.google.com");
+            }
+        }
+    }
+}
